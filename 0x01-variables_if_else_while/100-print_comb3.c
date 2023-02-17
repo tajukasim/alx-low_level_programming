1af75cf9cd88a1467202f0d1da8520af59079e58
tajukasim <taju7798@gmail.com>
2023-02-17 23:48:30 +0300

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (a != b)
+	{
+	putchar(a);
+	putchar(b);
+	if (a == '8' && b == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

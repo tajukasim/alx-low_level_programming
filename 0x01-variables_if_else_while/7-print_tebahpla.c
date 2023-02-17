aa68f79acf7dcd42746e55382df31a966d86d7e3
tajukasim <taju7798@gmail.com>
2023-02-17 23:39:28 +0300

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

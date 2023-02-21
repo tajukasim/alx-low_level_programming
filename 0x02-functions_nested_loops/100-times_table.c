2aa2a17906de3c217e49e487c71e26541eaadd4c
tajukasim <taju7798@gmail.com>
2023-02-21 20:40:52 +0300

    commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for j = 0; j <= n; j++)
+			printf("%d", i * j);
+
+	return (0);
+}

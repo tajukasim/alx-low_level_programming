2cf89ed1ae75c7f39196e9409162e6241e52f37d
tajukasim <taju7798@gmail.com>
2023-03-07 22:16:26 +0300

    task -7

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,19 +6,13 @@
 
  * @s: starting address of memory to be filled
 
- * @b: the desired value
-
- * @n: number of bytes to be changed
-
  *
 
  * Return: changed array with new value for n bytes
 
  */
 
-char *_memset(char *s, char b, unsigned int n)
-
-{
+char *_memset(char *s, char b, unsigned int n){
 
         int i = 0;
 
@@ -31,5 +25,4 @@ char *_memset(char *s, char b, unsigned int n)
         }
 
         return (s);
-
 }

f38017a0f9ef9f2973b150fcf33936eb1bb0efa9
tajukasim <taju7798@gmail.com>
2023-03-11 00:11:17 +0300

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints the number of arguments passed to it
+ * @argc: counts of arguements on command line
+ * @argv: pointer to the array of command line arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+printf("%i\n", argc - 1);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the of two arguments passed to it
+ * @argc: counts of arguements on command line
+ * @argv: pointer to the array of command line arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int product;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+product = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", product);
+return (0);
+}

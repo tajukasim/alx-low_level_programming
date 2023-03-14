c1fd66420e7a8cf83119842b278da95cc2a82f0c
tajukasim <taju7798@gmail.com>
2023-03-14 20:56:45 +0300

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *pstr;
-	unsigned int i;
-
-	pstr = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
-		pstr[i] = c;
-	pstr[i] = 0;
-	return (size < 1 ? 0 : pstr);
+	char *str;
+        unsigned int i;
+        str = malloc(sizeof(char) * size);
+        if (size == 0 || str == NULL)
+                return (NULL);
+        for (i = 0; i < size; i++)
+                str[i] = c;
 }

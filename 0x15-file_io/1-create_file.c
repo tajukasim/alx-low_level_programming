644261821af786af190a147b20ba4fbca041e78a
taju kasim <taju7798@gmail.com>
2023-05-11 09:20:07 +0300

    this is alx file project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @text_content: A pointer to a string to file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.

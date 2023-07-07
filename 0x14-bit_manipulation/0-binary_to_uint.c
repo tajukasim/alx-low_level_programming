a782e84c2b1dc73a8879b64eb63b70b7230481c1
taju kasim <taju7798@gmail.com>
2023-07-07 10:03:41 +0300

    hotfix6

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0;
-	int len, i;
-	int decval = 1;
+        int i;
+        unsigned int dec_val = 0;
 
-	if (!b)
-		return (0);
+        if (!b)
+                return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+        for (i = 0; b[i]; i++)
+        {
+                if (b[i] < '0' || b[i] > '1')
+                        return (0);
+                dec_val = 2 * dec_val + (b[i] - '0');
+        }
 
-	i = (len - 1);
-	for (; i >= 0; i--)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
-		if (b[i] == '1')
-			total += decval;
-		decval *= 2;
-	}
-
-	return (total);
+        return (dec_val);
 }
+

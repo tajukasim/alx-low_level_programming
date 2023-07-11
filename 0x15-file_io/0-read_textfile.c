cf1aaa7ed2c69ffc1fa00d7e801df2e314b90190
taju kasim <taju7798@gmail.com>
2023-07-11 06:46:54 +0300

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,23 +8,22 @@
  * Return: w- actual number of bytes read and printed
  * 0 when function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        char *buf;
-        ssize_t fp;
-        ssize_t wri;
-        ssize_t tt;
+	char *buf;
+	ssize_t fp;
+	ssize_t wri;
+	ssize_t tt;
 
-        fp = open(filename, O_RDONLY);
-        if (fp == -1)
-                return (0);
-        buf = malloc(sizeof(char) * letters);
-        tt = read(fp, buf, letters);
-        wri = write(STDOUT_FILENO, buf, tt);
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	tt = read(fp, buf, letters);
+	wri = write(STDOUT_FILENO, buf, tt);
 
-        free(buf);
-        close(fp);
-        return (wri);
+	free(buf);
+	close(fp);
+	return (wri);
 }
 
